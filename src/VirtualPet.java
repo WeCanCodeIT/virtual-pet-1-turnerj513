b3d9c260b15c5822fe42a6712400509ff8ee293d
Jay Turner <turnerj513@gmail.com>
2023-12-16 08:46:00 -0500

    i thought i committed a week ago. I had a empty commit message

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,3 +1,52 @@
-public class VirtualPet{
+public class VirtualPet {
+    private String name;
+    private int hunger;
+    private int thirst;
+    private int boredom;
 
+    public VirtualPet(String name) {
+        this.name = name;
+        this.hunger = 50;
+        this.thirst = 50;
+        this.boredom = 50;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public int getThirst() {
+        return thirst;
+    }
+
+    public int getBoredom() {
+        return boredom;
+    }
+
+    public void feed() {
+        hunger -= 10;
+        thirst += 5;
+        boredom += 5;
+    }
+
+    public void water() {
+        thirst -= 10;
+        boredom += 5;
+    }
+
+    public void play() {
+        boredom -= 10;
+        hunger += 5;
+        thirst += 5;
+    }
+
+    public void tick() {
+        hunger += 5;
+        thirst += 5;
+        boredom += 5;
+    }
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class VirtualPetApp {
     public static void main(String[] args) {
-        
+        VirtualPet pet = new VirtualPet("Jimmer");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the Virtual Pet Game!");
+        System.out.println("Your pet, " + pet.getName() + ", is ready to play.\n");
+
+        while (true) {
+            System.out.println("Pet's status:");
+            displayPetStatus(pet);
+
+            System.out.println("What would you like to do next?");
+            System.out.println("1. Feed the pet");
+            System.out.println("2. Give water to the pet");
+            System.out.println("3. Play with the pet");
+            System.out.println("4. Quit");
+
+            int choice = input.nextInt();
+            input.nextLine(); 
+
+            switch (choice) {
+                case 1:
+                    pet.feed();
+                    break;
+                case 2:
+                    pet.water();
+                    break;
+                case 3:
+                    pet.play();
+                    break;
+                case 4:
+                    System.out.println("Goodbye!");
+                    input.close(); 
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+            }
+
+            pet.tick();
+        }
+    }
+
+    private static void displayPetStatus(VirtualPet pet) {
+        System.out.println("Name: " + pet.getName());
+        System.out.println("Hunger: " + pet.getHunger());
+        System.out.println("Thirst: " + pet.getThirst());
+        System.out.println("Boredom: " + pet.getBoredom());
+        System.out.println();
     }
 }
